e86413a65e27e68226eebd18e3459a0370988024
Vinit Acharya <vin0itall@gmail.com>
2019-11-17 22:09:44 -0700

    updated

--- Client.py
+++ Client.py
@@ -6,4 +6,4 @@ with open('Input/example.json', 'r') as myfile:
     data=myfile.read()
 res = requests.post('http://localhost:5000/', json = data)
 if res.ok:
-    print (res)
+    print (res.content)
--- Server.py
+++ Server.py
@@ -1,7 +1,8 @@
 import numpy as np
+import os
 import pandas
 import tensorflow as tf
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import json
 import csv
 import Utils
@@ -13,50 +14,61 @@ from sklearn.externals.joblib import dump, load
 
 app = Flask(__name__)
 
+labels = {0: 'book', 1: 'car', 2: 'gift', 3: 'movie', 4: 'sell', 5: 'total'}
+ouptut = {}
+
 def convert_to_csv(d):
-    columns = ['score_overall', 'nose_score', 'nose_x', 'nose_y', 'leftEye_score', 'leftEye_x', 'leftEye_y',
-               'rightEye_score', 'rightEye_x', 'rightEye_y', 'leftEar_score', 'leftEar_x', 'leftEar_y',
-               'rightEar_score', 'rightEar_x', 'rightEar_y', 'leftShoulder_score', 'leftShoulder_x', 'leftShoulder_y',
-               'rightShoulder_score', 'rightShoulder_x', 'rightShoulder_y', 'leftElbow_score', 'leftElbow_x',
-               'leftElbow_y', 'rightElbow_score', 'rightElbow_x', 'rightElbow_y', 'leftWrist_score', 'leftWrist_x',
-               'leftWrist_y', 'rightWrist_score', 'rightWrist_x', 'rightWrist_y', 'leftHip_score', 'leftHip_x',
-               'leftHip_y', 'rightHip_score', 'rightHip_x', 'rightHip_y', 'leftKnee_score', 'leftKnee_x', 'leftKnee_y',
-               'rightKnee_score', 'rightKnee_x', 'rightKnee_y', 'leftAnkle_score', 'leftAnkle_x', 'leftAnkle_y',
-               'rightAnkle_score', 'rightAnkle_x', 'rightAnkle_y']
-    data = json.loads(d)
-    csv_data = np.zeros((len(data), len(columns)))
-    for i in range(csv_data.shape[0]):
-        one = []
-        one.append(data[i]['score'])
-        for obj in data[i]['keypoints']:
-            one.append(obj['score'])
-            one.append(obj['position']['x'])
-            one.append(obj['position']['y'])
-        csv_data[i] = np.array(one)
-    pd.DataFrame(csv_data, columns=columns).to_csv('Res/input.csv', index_label='Frames#')
+    try:
+        columns = ['score_overall', 'nose_score', 'nose_x', 'nose_y', 'leftEye_score', 'leftEye_x', 'leftEye_y',
+                   'rightEye_score', 'rightEye_x', 'rightEye_y', 'leftEar_score', 'leftEar_x', 'leftEar_y',
+                   'rightEar_score', 'rightEar_x', 'rightEar_y', 'leftShoulder_score', 'leftShoulder_x', 'leftShoulder_y',
+                   'rightShoulder_score', 'rightShoulder_x', 'rightShoulder_y', 'leftElbow_score', 'leftElbow_x',
+                   'leftElbow_y', 'rightElbow_score', 'rightElbow_x', 'rightElbow_y', 'leftWrist_score', 'leftWrist_x',
+                   'leftWrist_y', 'rightWrist_score', 'rightWrist_x', 'rightWrist_y', 'leftHip_score', 'leftHip_x',
+                   'leftHip_y', 'rightHip_score', 'rightHip_x', 'rightHip_y', 'leftKnee_score', 'leftKnee_x', 'leftKnee_y',
+                   'rightKnee_score', 'rightKnee_x', 'rightKnee_y', 'leftAnkle_score', 'leftAnkle_x', 'leftAnkle_y',
+                   'rightAnkle_score', 'rightAnkle_x', 'rightAnkle_y']
+        data = json.loads(d)
+        csv_data = np.zeros((len(data), len(columns)))
+        for i in range(csv_data.shape[0]):
+            one = []
+            one.append(data[i]['score'])
+            for obj in data[i]['keypoints']:
+                one.append(obj['score'])
+                one.append(obj['position']['x'])
+                one.append(obj['position']['y'])
+            csv_data[i] = np.array(one)
+        pd.DataFrame(csv_data, columns=columns).to_csv('Res/input.csv', index_label='Frames#')
+    except TypeError:
+        pass
 
 def loadModels(test_data):
-    model = tf.keras.models.load_model('Models/CNN.h5')
-    prediction = np.argmax(model.predict(np.array([test_data, ])))
-    print(prediction)
-    return str(prediction)
-
-
-
-output = [{ "CNN_Prediction" : 'Sell'},{ "NB_Prediction" : 'Total'}]
-output = str(output)
+    model1 = tf.keras.models.load_model('Models/CNN_1.h5')
+    model2 = tf.keras.models.load_model('Models/CNN_2.h5')
+    model3 = tf.keras.models.load_model('Models/CNN_3.h5')
+    model4 = tf.keras.models.load_model('Models/CNN_4.h5')
+    predictions=[]
+    prediction1 = np.argmax(model1.predict(np.array([test_data, ])))
+    prediction2 = np.argmax(model2.predict(np.array([test_data, ])))
+    prediction3 = np.argmax(model3.predict(np.array([test_data, ])))
+    prediction4 = np.argmax(model4.predict(np.array([test_data, ])))
+    predictions.append(labels[prediction1])
+    predictions.append(labels[prediction2])
+    predictions.append(labels[prediction3])
+    predictions.append(labels[prediction4])
+    return predictions
 
 @app.route('/', methods=['GET', 'POST'])
 def add_message():
-    result=''
-    try:
-        data = request.json
-        convert_to_csv(data)
-        test_data = Utils.preprocess()
-        result = loadModels(test_data)
-    except TypeError:
-        pass
-    return send_response(result)
+    data = request.get_json()
+    convert_to_csv(data)
+    test_data = Utils.preprocess()
+    os.remove('Res/input.csv')
+    predictions = loadModels(test_data)
+    x = {"0" : predictions[0],"1": predictions[1],"2":predictions[3],"3":predictions[3]}
+    return send_response(x)
+
+
 
 
 def send_response(x):
@@ -64,6 +76,5 @@ def send_response(x):
     return jsonRes
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host= '127.0.0.1')
+    app.run(host= '127.0.0.1',port=5000)
 
