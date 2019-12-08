e7f57850297ecada4ff4bf6123207f278852c279
Vin-itall <vin0itall@gmail.com>
2019-12-08 12:04:40 +0530

    Object detection

--- Client.py
+++ Client.py
@@ -1,9 +1,10 @@
 import requests
 import json
 from flask import jsonify
-
-with open('Input/example.json', 'r') as myfile:
-    data=myfile.read()
-res = requests.post('http://127.0.0.1:6000/', json = data)
-if res.ok:
-    print (res.content)
+for i in range(60):
+    print(i)
+    with open('Input/example.json', 'r') as myfile:
+        data=myfile.read()
+    res = requests.post('http://34.68.37.211:8000/', json = data)
+    if res.ok:
+        print (res.content)
